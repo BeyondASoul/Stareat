aa0fe740526cd35124f7394ae8872662821d5766
Alejandro Silva <alejandrosilva@AlexMacBook-Pro.local>
2020-11-07 22:26:40 -0600

    Minimal

--- lib/Auth/sign_in.dart
+++ lib/Auth/sign_in.dart
@@ -2,31 +2,35 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+// Instance creation
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
+// Variables to keep account data
 String name;
 String email;
 String imageUrl;
 
+//Future to sign in with Google
 Future<String> signInWithGoogle() async {
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); // Initializes firebase
 
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAccount googleSignInAccount =
+      await googleSignIn.signIn(); //Awaits for sign in
   final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+      await googleSignInAccount.authentication; //Authentication
 
   final AuthCredential credential = GoogleAuthProvider.credential(
     accessToken: googleSignInAuthentication.accessToken,
     idToken: googleSignInAuthentication.idToken,
-  );
+  ); //Credentials
 
   final UserCredential authResult =
       await _auth.signInWithCredential(credential);
   final User user = authResult.user;
 
   if (user != null) {
-    // Checking if email and name is null
+    // Checking if email and/or name are null
     assert(user.email != null);
     assert(user.displayName != null);
     assert(user.photoURL != null);
@@ -54,6 +58,7 @@ Future<String> signInWithGoogle() async {
   return null;
 }
 
+// Future to sign out from Google
 Future<void> signOutGoogle() async {
   await googleSignIn.signOut();
 
